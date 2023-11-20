cbeb31162fe554ebca0945eee0772550897315ba
Anna Ahmed <ahmeda66@cardiff.ac.uk>
2023-11-20 15:30:41 +0000

    project intialized

---
+++ BipSyncRecuritment/src/main/java/com/BipSyncRecuritment/BipSyncRecuritmentApplication.java
@@ -0,0 +1,13 @@
+package com.BipSyncRecuritment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BipSyncRecuritmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BipSyncRecuritmentApplication.class, args);
+	}
+
+}
---
+++ BipSyncRecuritment/src/test/java/com/BipSyncRecuritment/BipSyncRecuritmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BipSyncRecuritment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BipSyncRecuritmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
