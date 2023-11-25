d90777134dc6d8dc212598583da21d735a4c3af0
Anna Ahmed <ahmeda66@cardiff.ac.uk>
2023-11-25 16:33:06 +0000

    staff login added

--- BipSyncRecuritment/src/main/resources/data.sql
+++ BipSyncRecuritment/src/main/resources/data.sql
@@ -6,7 +6,14 @@ insert into users(username, password, user_first_name,user_last_name)
 values ('test', '$2a$12$4ns3D5/4F9k2YhkBO19onejm.DIhCZC/xyxJnRdMkzQm7b1BjBSEO','test','test');
 insert into user_roles(role_id, user_role)
 values(1,'ADMIN');
-
+insert into users(username, password, user_first_name,user_last_name)
+values ('Ben123', '$2a$12$KweLqQ1f/FpmdYmWD9hoq.h0TkNXOA1OKV0UJ8d.rNENY1UbdDqRS','Ben','Jones');
+insert into user_roles(role_id, user_role)
+values(1,'STAFF');
+insert into users(username, password, user_first_name,user_last_name)
+values ('Emma123', '$2a$12$i/zf8oYbgUooewlV8RfbjOGDcePzB3CP8OaZe75IDZMVSsCQUpj8a','Emma','Williams');
+insert into user_roles(role_id, user_role)
+values(1,'STAFF');
 
 
 
