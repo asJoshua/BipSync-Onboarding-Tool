3e91cbd0506fe13d797c955ed096d8ad6d08ec2f
Joshua Sadleir <sadleirj@cardiff.ac.uk>
2023-11-29 12:55:33 +0000

    Completed landing page and buttons

--- BipSyncRecuritment/src/main/java/com/BipSyncRecuritment/landingPage/LandingPageController.java
+++ BipSyncRecuritment/src/main/java/com/BipSyncRecuritment/landingPage/LandingPageController.java
@@ -1,7 +1,6 @@
 package com.BipSyncRecuritment.landingPage;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -9,29 +8,9 @@ import java.util.List;
 
 @RestController
 public class LandingPageController {
-    private NewRecruitService newRecruitService;
     @GetMapping("/home")
     public ModelAndView getHome(){
         ModelAndView modelAndView = new ModelAndView("home/home");
-        return modelAndView;
-    }
-
-    @GetMapping("/home/newRecruit")
-    public ModelAndView addNewRecruit(){
-        ModelAndView modelAndView = new ModelAndView("home/newRecruit");
-        List<NewRecruit> newRecruit = newRecruitService.getNewRecruits();
-        modelAndView.addObject("newRecruit", newRecruit);
-        return modelAndView;
-    }
-
-    @PostMapping("/home/newRecruit")
-    public ModelAndView addNewRecruit(NewRecruit newRecruit){
-
-        NewRecruitService newRecruitService = NewRecruitService.getInstance();
-        newRecruitService.addNewRecruit(newRecruit);
-
-        ModelAndView modelAndView = new ModelAndView("redirect:/home");
-        modelAndView.addObject("newRecruit", newRecruit);
         return modelAndView;
     }
 }
