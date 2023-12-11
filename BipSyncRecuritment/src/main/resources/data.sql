e198f7874d4b571ca90a1a96bd1313261c6f5b09
Anna Ahmed <ahmeda66@cardiff.ac.uk>
2023-12-11 17:31:08 +0000

    employee list

--- BipSyncRecuritment/src/main/resources/data.sql
+++ BipSyncRecuritment/src/main/resources/data.sql
@@ -18,4 +18,4 @@ values ('Ava', 'Patel', '2000/08/03', '07943567892', '768798678', 'AL097869C', '
 insert into recruits(first_name, last_name, date_of_birth, phone_number, passport_number, national_insurance_number, email, position, date_of_hire, emergency_contact_name, emergency_contact_phone)
 values ('Noah', 'Davis', '1998/04/03', '07957897895', '458798698', 'AW092869B', 'ndavis@bipsync.com', 'Data Analyst', '2022/06/12', 'Sarah', '07657465812');
 insert into recruits(first_name, last_name, date_of_birth, phone_number, passport_number, national_insurance_number, email, position, date_of_hire, emergency_contact_name, emergency_contact_phone)
-values ('Harper', 'Mitchell', '1997/10/18', '07865434545', '765678932', 'JW134678D', 'apatel@bipsync.com', 'Software Engineer', '2023/07/14', 'Josh', '079345789');
+values ('Harper', 'Mitchell', '1997/10/18', '07865434545', '765678932', 'JW134678D', 'hmitchell@bipsync.com', 'Software Engineer', '2023/07/14', 'Josh', '079345789');
--- BipSyncRecuritment/src/main/resources/schema.sql
+++ BipSyncRecuritment/src/main/resources/schema.sql
@@ -1,3 +1,4 @@
+
 drop table if exists user_roles;
 drop table if exists users;
 drop table if exists recruits;
